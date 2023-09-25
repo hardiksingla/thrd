28db0410d263cbce39906b16a746f176f8246461
hardiksingla <07hardiksingla@gmail.com>
2023-09-25 23:53:15 +0530

    changes

--- app.js
+++ app.js
@@ -6,19 +6,5 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
     clickable: true,
   },
-  breakpoints: {
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 4,
-      spaceBetween: 40,
-    },
-    1024: {
-      slidesPerView: 5,
-      spaceBetween: 50,
-    },
-  }
 });
 
